aec4982d4ab2f301ce1a16651479215f805223b7
Waiting7777 <waiting0819@msn.com>
2019-05-04 00:32:54 +0800

    Updates

---
+++ precache-manifest.9b4c99e9cdd26b10d74463cf0186f5a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cccb6217fff98216dad8c76d19ec3b5",
+    "url": "/poker/index.html"
+  },
+  {
+    "revision": "fe22ec2963eb43e7bb1f",
+    "url": "/poker/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/poker/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fe22ec2963eb43e7bb1f",
+    "url": "/poker/static/js/main.f0671924.chunk.js"
+  },
+  {
+    "revision": "fbdb2beac522419b5975",
+    "url": "/poker/static/js/runtime~main.f601251a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poker/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.380adf610922866d450d1a7155a60b54.js"
+  "/poker/precache-manifest.9b4c99e9cdd26b10d74463cf0186f5a7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/poker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
